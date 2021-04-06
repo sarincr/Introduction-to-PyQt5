8c157df49da0d6ae7252753cfc231855e9daf59d
sarincr <sarinrays@gmail.com>
2021-04-06 09:30:43 +0530

    Label

--- 05.Label.py
+++ 05.Label.py
@@ -1,26 +1,38 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel,QPushButton
 
-def clicked_button():
-    print("Clicked")
 
-def main():
-        
+class Class_Window(QMainWindow):
+    def __init__(self):
+        super(Class_Window,self).__init__()
+        self.GUI_Function()
+
+    def button_clicked(self):
+        self.label.setText("Hello, You have pressed")
+        self.Adj_Size()
+
+    def GUI_Function(self):
+        self.setGeometry(200, 200, 200, 200)
+        self.setWindowTitle("Button and Label")
+
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("Hello, Welcome!")
+        self.label.move(50,50)
+
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText("Click")
+        self.b1.clicked.connect(self.button_clicked)
+
+    def Adj_Size(self):
+        self.label.adjustSize()
+
+
+def window():
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(200, 200, 200, 200)
-    win.setWindowTitle("GUI")
+    win = Class_Window()
     
-    lab = QLabel(win)
-    lab.setText("Hello World")
-    lab.move(50,50)
-    
-    btn = QPushButton(win)
-    btn.setText("Hello World")
-    btn.clicked.connect(clicked_button)
     win.show()
     sys.exit(app.exec_())
 
-
-if __name__ == '__main__':
-    main()
+window()
